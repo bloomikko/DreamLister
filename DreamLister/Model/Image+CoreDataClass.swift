6873bb68949ca5db2d8d4775c3f182208dc2879b
Mikko Rouru <mikkorouru@Mikko-Mac-mini.local>
2017-12-21 11:26:52 +0200

    Created the project

---
+++ DreamLister/Model/Image+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Mikko Rouru on 21.12.2017.
+//  Copyright © 2017 Mikko Rouru. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Image)
+public class Image: NSManagedObject {
+
+}
---
+++ DreamLister/Model/ItemType+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Mikko Rouru on 21.12.2017.
+//  Copyright © 2017 Mikko Rouru. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ItemType)
+public class ItemType: NSManagedObject {
+
+}
